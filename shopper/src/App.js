bb7b71bb693285969e72f47203d7b2c94e4588cf
Carlos Akión Garro Campos <48141593+AkionGarro@users.noreply.github.com>
2023-09-08 22:28:53 -0600

    Login Added

--- shopper/src/App.js
+++ shopper/src/App.js
@@ -1,15 +1,11 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
-import Login from "./components/Login/Login";
 import PedidoOnline from "./Pages/PedidoOnline/PedidoOnline";
-
+import Login from "./Pages/Login/Login";
 function App() {
   return (
     <Router className="App">
       <Routes>
-        {/*Acá van las rutas en este estilo:
-<Route path="/" element={<MainPage />}/>
-*/}
         <Route path="/" element={<Login />} />
         <Route path="/PedidoOnline" element={<PedidoOnline />} />
       </Routes>
