b7c2d59e58811ffa1f02fc29142011d74ff75f0c
Bryam Steven López Miranda <bryamlopez1882002@estudianTEC.cr>
2023-09-11 00:22:15 -0600

    Trabajando en corregir navigador, no detecta una función en responsive

--- shopper/src/Pages/HomePageAdmin/HomePageAdmin.js
+++ shopper/src/Pages/HomePageAdmin/HomePageAdmin.js
@@ -7,7 +7,18 @@ import Typography from "@mui/material/Typography";
 import Link from "@mui/material/Link";
 import Navigator from "../../components/HomePage/Navigator";
 import Header from "../../components/HomePage/Header";
-
+import AppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import Grid from "@mui/material/Grid";
+import IconButton from "@mui/material/IconButton";
+import MenuIcon from "@mui/icons-material/Menu";
+import Avatar from "@mui/material/Avatar";
+import './HomePageAdmin.css'
+import Orders from "../Orders/Orders.jsx"
+import Products from "../Products/Products.jsx"
+import Categories from "../Categories/Categories.jsx"
+import NewAdmin from "../NewAdmin/NewAdmin.jsx";
+import Account from "../Account/Account.jsx";
 
 function Copyright() {
   return (
@@ -196,11 +207,18 @@ function Contact() {
 export default function HomePageAdmin(props) {
   const [mobileOpen, setMobileOpen] = React.useState(false);
   const isSmUp = useMediaQuery(theme.breakpoints.up("sm"));
+  const [componentToRender, setComponentToRender] = React.useState(<Orders />);
+  const [tittle, setTittle] = React.useState("Manage Orders");
 
   const handleDrawerToggle = () => {
     setMobileOpen(!mobileOpen);
   };
 
+  const navigatorOptions = (route, tittle) => {
+    setComponentToRender(route);
+    setTittle(tittle);
+  };
+ 
   return (
     <ThemeProvider theme={theme}>
       <Box sx={{ display: "flex", minHeight: "100vh" }}>
@@ -219,17 +237,57 @@ export default function HomePageAdmin(props) {
           )}
 
           <Navigator
-            PaperProps={{ style: { width: drawerWidth } }}
+            PaperProps={{ style: { width: drawerWidth }}} navigatorOptions={navigatorOptions}
             sx={{ display: { sm: "block", xs: "none" } }}
           />
         </Box>
         <Box sx={{ flex: 1, display: "flex", flexDirection: "column" }}>
-          <Header onDrawerToggle={handleDrawerToggle} />
+        <AppBar color="primary" position="sticky" elevation={0}>
+        
+        <Toolbar>
+          <Grid container spacing={1} alignItems="center">
+            <Grid sx={{ display: { sm: 'none', xs: 'block' } }} item>
+              <IconButton
+                color="inherit"
+                aria-label="open drawer"
+                onClick={handleDrawerToggle}
+                edge="start"
+              >
+                <MenuIcon />
+              </IconButton>
+            </Grid>
+            <Grid item xs />
+
+            <Grid item>
+              <IconButton color="inherit" sx={{ p: 0.5 }}>
+                <Avatar src="/static/images/avatar/1.jpg" alt="My Avatar" />
+              </IconButton>
+            </Grid>
+          </Grid>
+        </Toolbar>
+      </AppBar>
+      <AppBar
+        component="div"
+        color="primary"
+        position="static"
+        elevation={0}
+        sx={{ zIndex: 0 }}
+      >
+        <Toolbar>
+          <Grid container alignItems="center" spacing={1}>
+            <Grid item xs>
+              <Typography color="inherit" variant="h5" component="h1">
+                {tittle}
+              </Typography>
+            </Grid>
+          </Grid>
+        </Toolbar>
+      </AppBar>
           <Box
             component="main"
             sx={{ flex: 1, py: 6, px: 4, bgcolor: "#eaeff1" }}
           >
-
+          {componentToRender}
           </Box>
           <Box component="footer" sx={{ p: 2, bgcolor: "#eaeff1" }}>
             <Copyright />
---
+++ shopper/src/Pages/Orders/Orders.jsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import { useState, useEffect } from 'react';
+
+import './Orders.css';
+
+function Orders() {
+    return (
+        <div className="container"> Orders</div>
+    );
+}
+
+export default Orders;
