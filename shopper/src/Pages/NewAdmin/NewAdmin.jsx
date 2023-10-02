aeb184b0bc40a1f96ba0516ee3afa97c646039d4
Carlos Akión Garro Campos <48141593+AkionGarro@users.noreply.github.com>
2023-10-02 00:50:39 -0600

    Update Filters Manage Users

--- shopper/src/Pages/NewAdmin/NewAdmin.jsx
+++ shopper/src/Pages/NewAdmin/NewAdmin.jsx
@@ -10,11 +10,20 @@ import TableHead from "@mui/material/TableHead";
 import TablePagination from "@mui/material/TablePagination";
 import TableRow from "@mui/material/TableRow";
 import AddIcon from "@mui/icons-material/Add";
+import ManageAccountsIcon from "@mui/icons-material/ManageAccounts";
 import { useFirebase } from "../../context/DatabaseContext";
 import "./NewAdmin.css";
 import Swal from "sweetalert2";
-import CloseOutlinedIcon from '@mui/icons-material/CloseOutlined';
-import CheckOutlinedIcon from '@mui/icons-material/CheckOutlined';
+import CloseOutlinedIcon from "@mui/icons-material/CloseOutlined";
+import AddCircleOutlineIcon from "@mui/icons-material/AddCircleOutline";
+import CheckOutlinedIcon from "@mui/icons-material/CheckOutlined";
+import {
+  FormControl,
+  InputLabel,
+  MenuItem,
+  Select,
+  TextField,
+} from "@mui/material";
 function NewAdmin() {
   const [users, setUsers] = useState([]);
   const api = useFirebase();
@@ -22,6 +31,17 @@ function NewAdmin() {
   const [rowsPerPage, setRowsPerPage] = React.useState(10);
   const [flagUpdate, setFlagUpdate] = useState(false);
   const [isColumnVisible, setIsColumnVisible] = useState(true);
+
+  const [searchText, setSearchText] = useState("");
+  const [filterUserType, setFilterUserType] = useState("");
+
+  const handleSearchChange = (event) => {
+    setSearchText(event.target.value);
+  };
+
+  const handleFilterChange = (event) => {
+    setFilterUserType(event.target.value);
+  };
 
   const handleChangePage = (event, newPage) => {
     setPage(newPage);
@@ -31,6 +51,16 @@ function NewAdmin() {
     setRowsPerPage(+event.target.value);
     setPage(0);
   };
+
+  const filteredUsers = users.filter((user) => {
+    const nameMatches = user.email
+      .toLowerCase()
+      .includes(searchText.toLowerCase());
+    const userTypeMatches =
+      filterUserType === "" || user.userType === filterUserType;
+
+    return nameMatches && userTypeMatches;
+  });
 
   const handleNewAdmin = (email) => {
     Swal.fire({
@@ -73,18 +103,24 @@ function NewAdmin() {
     if (props.userData.userType === "admin") {
       return (
         <div className="container__permissions">
-        <Button onClick={() => changeAdminToUser(props.userData.email)}>
-          <CheckOutlinedIcon></CheckOutlinedIcon> 
-        </Button>
+          <Button onClick={() => changeAdminToUser(props.userData.email)}>
+            <div className="boxButton">
+              <ManageAccountsIcon></ManageAccountsIcon>
+               Editar
+            </div>
+          </Button>
         </div>
-
       );
     } else {
       return (
         <div className="container__permissions">
-        <Button onClick={() => handleNewAdmin(props.userData.email)}>
-        <CloseOutlinedIcon></CloseOutlinedIcon> 
-        </Button>
+          <Button onClick={() => handleNewAdmin(props.userData.email)}>
+          <div className="boxButton">
+          <AddCircleOutlineIcon></AddCircleOutlineIcon>
+              Agregar 
+            </div>
+            
+          </Button>
         </div>
       );
     }
@@ -112,10 +148,8 @@ function NewAdmin() {
     fetchData();
   }, [flagUpdate]);
 
-
   useEffect(() => {
     const handleResize = () => {
-  
       if (window.innerWidth < 900) {
         setIsColumnVisible(false);
       } else {
@@ -123,36 +157,70 @@ function NewAdmin() {
       }
     };
 
-    window.addEventListener('resize', handleResize);
+    window.addEventListener("resize", handleResize);
     handleResize();
 
     return () => {
-      window.removeEventListener('resize', handleResize);
+      window.removeEventListener("resize", handleResize);
     };
   }, []);
 
   return (
     <Paper sx={{ width: "100%", overflow: "hidden" }}>
+      <TextField
+        label="Buscar usuario"
+        variant="outlined"
+        value={searchText}
+        onChange={handleSearchChange}
+        style={{ margin: "0.5rem" }}
+      />
+
+      <FormControl
+        variant="outlined"
+        style={{ margin: "0.5rem", width: "7rem" }}
+      >
+        <InputLabel>Tipo</InputLabel>
+        <Select
+          value={filterUserType}
+          onChange={handleFilterChange}
+          label="User Type"
+        >
+          <MenuItem value="">No filtrar</MenuItem>
+          <MenuItem value="admin">Admin</MenuItem>
+          <MenuItem value="user">User</MenuItem>
+        </Select>
+      </FormControl>
+
       <TableContainer sx={{ maxHeight: 500 }}>
         <Table stickyHeader aria-label="sticky table">
           <TableHead>
             <TableRow>
               <TableCell>Email</TableCell>
-              {isColumnVisible &&<TableCell>Nombre Completo</TableCell>}
-                {isColumnVisible &&<TableCell>Teléfono</TableCell>}
-                {isColumnVisible &&<TableCell>Tipo</TableCell>}
-              <TableCell>Administrador</TableCell>
+              {isColumnVisible && <TableCell>Cédula</TableCell>}
+              {isColumnVisible && <TableCell>Nombre Completo</TableCell>}             
+              {isColumnVisible && <TableCell>Teléfono</TableCell>}
+              {isColumnVisible && <TableCell>Tipo</TableCell>}
+              <TableCell>Permisos</TableCell>
             </TableRow>
           </TableHead>
           <TableBody>
-            {users
+            {filteredUsers
               .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
               .map((user, index) => (
                 <TableRow key={user.email} hover>
                   <TableCell className="email-cell">{user.email}</TableCell>
-                  {isColumnVisible && <TableCell className="name-cell">{user.fullName}</TableCell>}
-                  {isColumnVisible &&<TableCell className="phone-cell">{user.phone}</TableCell>}
-                  {isColumnVisible &&<TableCell className="type-cell">{user.userType}</TableCell>}
+                  {isColumnVisible && (
+                    <TableCell className="name-cell">{user.identification}</TableCell>
+                  )}
+                  {isColumnVisible && (
+                    <TableCell className="name-cell">{user.fullName}</TableCell>
+                  )}
+                  {isColumnVisible && (
+                    <TableCell className="phone-cell">{user.phone}</TableCell>
+                  )}
+                  {isColumnVisible && (
+                    <TableCell className="type-cell">{user.userType}</TableCell>
+                  )}
                   <TableCell className="permissions-cell">
                     <UserPermissions userData={user}></UserPermissions>
                   </TableCell>
