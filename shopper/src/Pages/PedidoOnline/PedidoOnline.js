d007fc71e74371448f0e7c5b3af7c082da40948d
DanielChen02 <danielchenm02@estudianTEC.cr>
2023-10-06 21:36:19 -0600

    Adding User Selector and Create User in Online Order

--- shopper/src/Pages/PedidoOnline/PedidoOnline.js
+++ shopper/src/Pages/PedidoOnline/PedidoOnline.js
@@ -1,21 +1,34 @@
 import React, { useState } from "react";
+import { useEffect } from "react";
 import {
   Button,
   Container,
   Grid,
   TextField,
+  Autocomplete,
   Select,
   MenuItem,
 } from "@mui/material";
 import { firestore } from "../../firebase";
 import { collection } from "firebase/firestore";
 import { addDocument } from "../../firebase";
+import PersonAddIcon from "@mui/icons-material/PersonAdd";
+import SearchIcon from "@mui/icons-material/Search";
+import { useFirebase } from "../../context/DatabaseContext";
 import DeleteIcon from "@mui/icons-material/Delete";
 import Swal from "sweetalert2";
 import { auth } from "../../firebase";
+import AddUserModalOnline from "./AddUserModalOnline";
 import "./PedidoOnline.css";
 
 const PedidoOnline = () => {
+  const api = useFirebase();
+  const [users, setUsers] = useState([]);
+  const [open, setOpen] = useState(false);
+  const [flagUpdate, setFlagUpdate] = useState(false);
+  const [searchQuery, setSearchQuery] = useState("");
+  const [selectedUser, setSelectedUser] = useState("");
+
   const ref = collection(firestore, "pedidosOnline");
   const [linkFields, setLinkFields] = useState([{ link: "", comentario: "" }]);
   const [direction, setDirection] = useState("");
@@ -26,11 +39,15 @@ const PedidoOnline = () => {
     setDirection("");
   };
 
+  const handleOpenModal = () => {
+    setOpen(true);
+  };
+
   const handleSubmit = (e) => {
     e.preventDefault();
 
     let data = {
-      usuario: user.email,
+      usuario: selectedUser.email,
       direccion: direction,
       productos: linkFields,
       estado: 0,
@@ -97,10 +114,31 @@ const PedidoOnline = () => {
     setLinkFields(updatedFields);
   };
 
-  const handleDirectionOnSelect = (event) => {
-    setDirection(event.target.value);
-    console.log(event.target.value);
-  };
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const querySnapshot = await api.getAllUsers();
+        const userData = [];
+        for (const doc in querySnapshot) {
+          userData.push(querySnapshot[doc]);
+        }
+        const filteredUsers = userData.filter((user) =>
+          user.email.toLowerCase().includes(searchQuery.toLowerCase())
+        );
+        setUsers(filteredUsers);
+        console.log(userData);
+      } catch (error) {
+        Swal.fire({
+          icon: "error",
+          title: "Error al obtener usuarios",
+          text: error,
+        });
+      }
+    };
+    setFlagUpdate(false);
+
+    fetchData();
+  }, [flagUpdate, searchQuery]);
 
   return (
     <Container className="container">
@@ -162,25 +200,58 @@ const PedidoOnline = () => {
         </Button>
       </div>
 
-      <div className="opciones-direccion">
-        <h4 className="texto">
-          Selecciona la dirección en la cual se entregaran tus productos
-        </h4>
-        <Select
-          fullWidth
-          variant="outlined"
-          value={direction}
-          onChange={handleDirectionOnSelect}
-          displayEmpty
-          id="seleccionar"
+      <div className="users_container">
+        <div className="opciones-direccion">
+          <Autocomplete
+            options={users}
+            getOptionLabel={(user) => user.email}
+            value={selectedUser}
+            onChange={(event, newValue) => {
+              setSelectedUser(newValue);
+              console.log("Usuario seleccionado");
+              console.log(newValue);
+            }}
+            renderInput={(params) => (
+              <TextField
+                {...params}
+                label="Buscar por correo electrónico"
+                variant="outlined"
+                fullWidth
+                InputProps={{
+                  ...params.InputProps,
+                  startAdornment: (
+                    <SearchIcon position="start" fontSize="small" />
+                  ),
+                }}
+                onChange={(e) => setSearchQuery(e.target.value)}
+              />
+            )}
+            isOptionEqualToValue={(option, value) =>
+              option.email === value.email
+            }
+            noOptionsText="No hay resultados"
+            PopperProps={{
+              anchorOrigin: {
+                vertical: "top",
+                horizontal: "right",
+              },
+              transformOrigin: {
+                vertical: "bottom",
+                horizontal: "right",
+              },
+            }}
+          />
+        </div>
+
+        <div
+          className="addUser__button"
+          onClick={() => {
+            handleOpenModal();
+          }}
         >
-          <MenuItem value="" disabled>
-            Seleccionar dirección
-          </MenuItem>
-          <MenuItem value="rojo">Rojo</MenuItem>
-          <MenuItem value="azul">Azul</MenuItem>
-          <MenuItem value="verde">Verde</MenuItem>
-        </Select>
+          <PersonAddIcon></PersonAddIcon>
+          <p>Agregar usuario</p>
+        </div>
       </div>
 
       <div className="boton-enviar">
@@ -193,6 +264,11 @@ const PedidoOnline = () => {
           Realizar pedido
         </Button>
       </div>
+      <AddUserModalOnline
+        open={open}
+        setOpen={setOpen}
+        setFlagUpdate={setFlagUpdate}
+      />
     </Container>
   );
 };
