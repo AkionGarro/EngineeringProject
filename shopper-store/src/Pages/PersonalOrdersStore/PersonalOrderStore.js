a9efd13f5aa5c222523741c375c3ddb2c4b43d28
Fiorela Sirias <61396158+Sirias2412@users.noreply.github.com>
2023-11-20 16:20:22 -0600

    Nombres en whats

--- shopper-store/src/Pages/PersonalOrdersStore/PersonalOrderStore.js
+++ shopper-store/src/Pages/PersonalOrdersStore/PersonalOrderStore.js
@@ -33,6 +33,7 @@ function Personal_Order() {
   const referencia = collection(firestore, "pedidosPersonales");
   const auth = useAuth();
   const email = auth.user.email;
+  const [actualName, setActualName] = useState("");
 
   useEffect(() => {
     const fetchData = async () => {
@@ -40,6 +41,12 @@ function Personal_Order() {
 
       const data = await firebase.getUserData(email);
       setDireccionSeleccionada(data.direccionEnvio);
+      if (data == undefined) {
+        let nameUser = auth.user.displayName;
+        setActualName(nameUser);
+      } else {
+        setActualName(data.fullName);
+      }
 
       const direcciones = await firebase.getUserAdress(email)
       setAddress(direcciones);
@@ -202,7 +209,7 @@ function Personal_Order() {
       "?text=" +
       encodeURIComponent(
         `*Pedido Personal*\n\n` +
-        `*Nombre:* ${email}\n\n` +
+        `*Nombre:* ${actualName}\n\n` +
         `*Productos:*\n${message}\n\n` +
         `_[Enviado desde la página web de VeroCam Shop]_`
       );
